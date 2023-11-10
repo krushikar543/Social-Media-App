eb132a2c30e8d6b385c43a7257add00c8eb26e1d
= <=>
2023-11-10 12:10:21 +0530

    Redux, ToolKit and total front-end set up done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "display/homePage";
+import LoginPage from "display/loginPage";
+import ProfilePage from "display/profilePage";
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<LoginPage />} />
+          <Route path="/home" element = {<HomePage />} />
+          <Route path="/profile/:id" element = {<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
